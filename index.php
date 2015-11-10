27e7718070726e706e98640497ada5cd3a4c0b2f
Yeonjae Lee <thisyj98@gmail.com>
2015-11-10 21:44:24 +0900

    add router

--- index.php
+++ index.php
@@ -9,6 +9,51 @@ getRoute()->get('/', 'showEndpoints');
 getRoute()->get('/version', 'showVersion');
 getRoute()->get('/users', 'getUserList',EpiApi::external);
 
+//전체 가져오기(최신순, wish순, 조회순)
+getRoute()->get('/items/recently/?(\d+)?','get_items_recently_list',EpiApi::external);
+getRoute()->get('/items/mostwish','get_items_mostwish_list',EpiApi::external);
+getRoute()->get('/items/mostview','get_items_mostview_list',EpiApi::external);
+
+//카테고리-패션의류 가져오기(최신순, wish순, 조회순)
+getRoute()->get('/fashionclothes/recently','get_fashionclothes_recently_list',EpiApi::external);
+getRoute()->get('/fashionclothes/mostwish','get_fashionclothes_mostwish_list',EpiApi::external);
+getRoute()->get('/fashionclothes/mostview','get_fashionclothes_mostview_list',EpiApi::external);
+
+//카테고리-패션잡화 가져오기(최신순, wish순, 조회순)
+getRoute()->get('/fashiongoods/recently','get_fashiongoods_recently_list',EpiApi::external);
+getRoute()->get('/fashiongoods/mostwish','get_fashiongoods_mostwish_list',EpiApi::external);
+getRoute()->get('/fashiongoods/mostview','get_fashiongoods_mostview_list',EpiApi::external);
+
+//카테고리-뷰티 가져오기(최신순, wish순, 조회순)
+getRoute()->get('/beautygoods/recently','get_beautygoods_recently_list',EpiApi::external);
+getRoute()->get('/beautygoods/mostwish','get_beautygoods_mostwish_list',EpiApi::external);
+getRoute()->get('/beautygoods/mostview','get_beautygoods_mostview_list',EpiApi::external);
+
+//카테고리-식품 가져오기(최신순, wish순, 조회순)
+getRoute()->get('/foods/recently','get_foods_recently_list',EpiApi::external);
+getRoute()->get('/foods/mostwish','get_foods_mostwish_list',EpiApi::external);
+getRoute()->get('/foods/mostview','get_foods_mostview_list',EpiApi::external);
+
+//카테고리-식품 가져오기(최신순, wish순, 조회순)
+getRoute()->get('/fancygoods/recently','get_fancygoods_recently_list',EpiApi::external);
+getRoute()->get('/fancygoods/mostwish','get_fancygoods_mostwish_list',EpiApi::external);
+getRoute()->get('/fancygoods/mostview','get_fancygoods_mostview_list',EpiApi::external);
+
+//좋아요 +1, -1 기능
+getRoute()->get('/post/(\d+)/product/wish','post_wish_plus',EpiApi::external);
+getRoute()->get('/post/(\d+)/product/unwish','post_wish_minus',EpiApi::external);
+
+//글을 누르면 보이는 상품(d는 post number)
+getRoute()->get('/post/(\d+)/products','get_product_list',EpiApi::external);
+
+//wishList (조회순, 최신순, wish순 없음, 넣은 순서대로)
+getRoute()->get('/wishitems','get_wishitems',EpiApi::external);
+
+//맞춤검색-> 
+getRoute()->post('/items/recently','search_items_recently_list',EpiApi::external);
+getRoute()->post('/items/mostwish','search_items_mostwish_list',EpiApi::external);
+getRoute()->post('/items/mostview','search_items_mostview_list',EpiApi::external);
+
 getRoute()->run();
 
 
@@ -43,6 +88,24 @@ function getUserList()
     array('name' => '허하진'),
     array('name' => '윤서영')
   );
+}
+
+
+function get_items_recently_list( $count = 10 ){
+	$items = array();
+
+	for($i = 0; $i < count; $i ++){
+		array_push( $items,
+				array(
+					'id' => $i,
+					'name' => '연미령',
+					'tel' => '010-1234-5678',
+					'address' => '서울시 관악구 호암로546(신림동)'
+				)
+		);
+	}
+	return $items;
+
 }
 
 ?>

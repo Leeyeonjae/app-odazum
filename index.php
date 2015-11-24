0e839a117a8c8d754f3e614a94693f0511153c16
Yeonjae Lee <thisyj98@gmail.com>
2015-11-24 20:37:15 +0900

    #6 Complete DB Query

--- index.php
+++ index.php
@@ -43,18 +43,18 @@ getRoute()->get('/fancygoods/mostwish/?(\d+)?','get_fancygoods_mostwish_list',Ep
 getRoute()->get('/fancygoods/mostview/?(\d+)?','get_fancygoods_mostview_list',EpiApi::external);
 
 //좋아요 +1, -1 기능
-getRoute()->get('/post/(\d+)/product/wish','post_wish_plus',EpiApi::external);
-getRoute()->get('/post/(\d+)/product/unwish','post_wish_minus',EpiApi::external);
+getRoute()->post('/post/product/wish','post_wish_plus',EpiApi::external);
+getRoute()->post('/post/product/unwish','post_wish_minus',EpiApi::external);
 
 //글을 누르면 보이는 상품(d는 post number)
-getRoute()->get('/post/(\d+)/products','get_product_list',EpiApi::external);
+getRoute()->post('/post/products','get_product_list',EpiApi::external);
 
 //wishList (조회순, 최신순, wish순 없음, 넣은 순서대로)
 getRoute()->post('/wishitems','get_wishitems',EpiApi::external);
 
 //맞춤검색-> 
 getRoute()->post('/items/?(\d+)?','search_items',EpiApi::external);
-
+getRoute()->post('/easysearch','easy_search',EpiApi::external);
 
 getRoute()->get('/randomtags/?(\d+)?','random_tags',EpiApi::external);
 getRoute()->run();
@@ -71,7 +71,7 @@ getRoute()->run();
 function get_items_recently_list( $count ){
 
 	//getDatabase()->execute('SET NAMES utf8');
-	$sql = 'SELECT id, title, image, click FROM posts as p order by id desc ';
+	$sql = 'SELECT id, title, image, click,wish FROM posts as p order by id desc ';
 	if($count > 0){
 		$sql .= ' limit ' . $count;
 	}
@@ -84,6 +84,7 @@ function get_items_recently_list( $count ){
 							'title' => $r['title'],
 							'image' => $r['image'],
 							'click' => $r['click'],
+							'wish' => $r['wish'],
 					)
 			);
 		}
@@ -445,21 +446,21 @@ function get_fancygoods_mostview_list($count){
 	return $items;
 }
 
-function post_wish_plus($post_id){
+function post_wish_plus(){
 	//getDatabase()->execute('SET NAMES utf8');
-	getDatabase()->execute('UPDATE posts SET wish = wish+1 where id = ' . $post_id );
+	getDatabase()->execute('UPDATE posts SET wish = wish+1 where id = ' .$_POST['post_id'] );
 
 }
-function post_wish_minus($post_id){
+function post_wish_minus(){
 	//getDatabase()->execute('SET NAMES utf8');
-	getDatabase()->execute('UPDATE posts SET wish = wish-1 where id = ' . $post_id );
+	getDatabase()->execute('UPDATE posts SET wish = wish-1 where id = ' .$_POST['post_id'] );
 
 }
 
-function get_product_list($post_id){
+function get_product_list(){
 	//getDatabase()->execute('SET NAMES utf8');
-	getDatabase()->execute('UPDATE posts SET click = click+1 where id = ' . $post_id );
-	$rs = getDatabase()->all('SELECT id, name,image,price,text FROM products WHERE post = ' .$post_id );
+	getDatabase()->execute('UPDATE posts SET click = click+1 where id = ' .$_POST['post_id'] );
+	$rs = getDatabase()->all('SELECT id, name,image,price,text FROM products WHERE post = '.$_POST['post_id'] );
 	$items = array();
 	foreach( $rs as $key => $r ){
 			array_push( $items,
@@ -479,7 +480,8 @@ function get_product_list($post_id){
 
 function get_wishitems(){
 	//getDatabase()->execute('SET NAMES utf8');
-	if($_POST['wish_id_list'].length < 1 ){
+	
+	if(strlen($_POST['wish_id_list'])< 1 ){
 		return  array();
 	}
 
@@ -503,34 +505,48 @@ function get_wishitems(){
 
 
 }
-/*
-function search_items($count){
 
-$_POST['age']
-$_POST['gender']
-$_POST['tag']
-$_POST['maxprice']
-$_POST['orderby']
+function search_items(){
+	//getDatabase()->execute('SET NAMES utf8');
 
-getDatabase()->execute('SET NAMES utf8');
-$rs = getDatabase()->all('SELECT DISTINCT p.id,p.title,p.image FROM posts p,posts_tag t, posts_gender g,posts_age a, products pro WHERE p.id = g.p_id and p.id = t.p_id  and p.id = a.p_id and p.id=pro.post and  g.g_id ='.$gender ' and a.a_id = '.$age ' and t.t_id  in('.$tag ' ) group by p.id HAVING min(pro.price) <= '.$maxprice ' limte ' . $count 'order by p.id');
+	/*
+	$_POST['age']
+	$_POST['gender']
+	$_POST['tag']
+	$_POST['maxprice']
+	$_POST['orderby']
+	*/
 
-$items = array();
-foreach( $rs as $key => $r ){
-		array_push( $items,
-				array(
-						'id' => $r['id'],
-						'title' => $r['title'],
-						'image' => $r['image'],
-				)
-		);
+	if(strlen($_POST['tag']) < 1 ){
+		$sql = 'SELECT DISTINCT p.id as id ,p.title as title ,p.image as image FROM posts p, posts_gender g,posts_age a, products pro WHERE p.id = g.p_id and p.id = a.p_id and p.id=pro.post and g.g_id= '.$_POST['gender'].' and a.a_id = ' . $_POST['age'] .' GROUP BY pro.post HAVING min(pro.price)<= '. $_POST['maxprice'] ;
+
+	}else{
+		$sql ='SELECT DISTINCT p.id as id ,p.title as title ,p.image as image FROM posts p,posts_tag t, posts_gender g,posts_age a, products pro where p.id = g.p_id and p.id = t.p_id and p.id = a.p_id and p.id=pro.post and  g.g_id= '.$_POST['gender'].' and a.a_id = '. $_POST['age'] .' and t.t_id  in( SELECT no FROM tag WHERE name in("'.$_POST['tag'].'")) GROUP BY p.id  HAVING min(pro.price)<= '. $_POST['maxprice'] ;
 	}
 
-return $items;
+	if($_POST['orderby'] == 1){
+		$sql .= ' order by p.click desc';
+	}else if($_POST['orderby']==2){
+		$sql .= ' order by p.id desc';
+	}
+
+
+	$rs = getDatabase()->all($sql);
+	$items = array();
+	foreach( $rs as $key => $r ){
+			array_push( $items,
+					array(
+							'id' => $r['id'],
+							'title' => $r['title'],
+							'image' => $r['image'],
+					)
+			);
+		}
+
+	return $items;
 
 }
 
-*/
 
 function random_tags( $count = 6 ){
 
@@ -552,5 +568,25 @@ function random_tags( $count = 6 ){
 
 	return $items;
 
+}
+
+function easy_search(){
+//getDatabase()->execute('SET NAMES utf8');
+$sql = 'SELECT DISTINCT p.id,p.title,p.image FROM posts p, products pro where p.id=pro.post and (p.title LIKE "%'.$_POST['word'].'%" or pro.text LIKE "%'.$_POST['word'].'%"); ' ;
+
+
+$rs = getDatabase()->all($sql);
+$items = array();
+foreach( $rs as $key => $r ){
+		array_push( $items,
+				array(
+						'id' => $r['id'],
+						'title' => $r['title'],
+						'image' => $r['image'],										
+				)
+		);
+	}
+
+return $items;
 }
 ?>
